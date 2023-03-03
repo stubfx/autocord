6ee7d65d81ef3594a137b2ab388f46d54f42b143
stubfx <14958556+stubfx@users.noreply.github.com>
2023-03-03 07:46:29 +0100

    Add tmp bg sample

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,6 +1,7 @@
 <template>
+  <div id="bg" class="absolute top-0 left-0 w-screen h-screen"></div>
   <div class="flex w-full h-full flex-col items-center absolute gap">
-    <div class="w-full">
+    <div class="absolute top-0 left-0 w-full">
       <app-container></app-container>
     </div>
     <div class="flex flex-grow w-full justify-center items-end gap">
@@ -14,4 +15,10 @@ import AppContainer from "./components/main/appContainer.vue";
 </script>
 
 <style scoped>
+#bg {
+  background-image: url("/src/assets/bg-sample.png");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
 </style>
