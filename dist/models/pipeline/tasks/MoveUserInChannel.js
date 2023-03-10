c9ced4f49b06aa32d6d81ebad693b3f92a60fb66
stubfx <14958556+stubfx@users.noreply.github.com>
2023-03-10 21:15:09 +0100

    Users can now be moved in another voice channel

---
+++ dist/models/pipeline/tasks/MoveUserInChannel.js
@@ -0,0 +1,25 @@
+import { Task } from "../Task.js";
+import { ChainLinkTypes } from "../chain/ChainLinkTypes.js";
+import { PermissionsBitField } from "discord.js";
+export class MoveUserInChannel extends Task {
+    name = ChainLinkTypes.Task.MoveUserInChannel;
+    description = 'Move the given user in the given channel.';
+    requiredPermissions = [PermissionsBitField.Flags.MoveMembers];
+    acceptParams = [{
+            name: "channelId",
+            type: ChainLinkTypes.Param.CHANNEL_ID
+        }, {
+            name: "userId",
+            type: ChainLinkTypes.Param.STRING
+        }];
+    async behavior(...args) {
+        let channelId = this.getResolvedParam("channelId");
+        let userId = this.getResolvedParam("userId");
+        let guild = await this.getFetchedGuild();
+        let guildMember = await guild.members.fetch(userId);
+        let channel = await guild.channels.fetch(channelId);
+        await guildMember.voice.setChannel(channel, "Move");
+        return true;
+    }
+}
+//# sourceMappingURL=MoveUserInChannel.js.map
