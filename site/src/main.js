ca5377fcd122a74936ead817b58b066ece5616bb
stubfx <14958556+stubfx@users.noreply.github.com>
2023-03-11 01:23:25 +0100

    Minor fix and improvements

--- site/src/main.js
+++ site/src/main.js
@@ -3,6 +3,9 @@ import './style.css'
 import App from './App.vue'
 import store from './store.js'
 import router from "./router.js";
+import {initTracker} from "../trackingUtils.js";
+
+initTracker()
 
 createApp(App)
     .use(store)
--- site/trackingUtils.js
+++ site/trackingUtils.js
@@ -1,7 +1,9 @@
 import posthog from 'posthog-js'
 import {isLocalhost} from "./utils.js";
 
-posthog.init('phc_s55wiylScPcHVymsaH9BmF430nnhDoFKiq03GakkW0M', { api_host: 'https://eu.posthog.com' })
+export function initTracker() {
+    posthog.init('phc_s55wiylScPcHVymsaH9BmF430nnhDoFKiq03GakkW0M', { api_host: 'https://eu.posthog.com' })
+}
 
 export function track(name, params) {
     if (!isLocalhost()) {
